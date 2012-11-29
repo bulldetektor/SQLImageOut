1a2482d70c958270215662e8e4743ed1dac6a938
ajoiner <ajoiner@mobilecomputing.uk.net>
2012-11-29 14:10:01 +0000

    Corrected license statement

--- Program.cs
+++ Program.cs
@@ -124,7 +124,7 @@ namespace RunSQLOLEDB
             message.AppendLine(@"
     Copyright 2012 Andrew Joiner
 
-Licensed under the Apache License, Version 2.0 (the "+"\""+@"License\""+@"+"\""+@");
+Licensed under the Apache License, Version 2.0 (the "+"\""+@"License"+"\""+@");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
